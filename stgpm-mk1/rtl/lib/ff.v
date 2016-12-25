bb33f58266dd560af72b4065b7ea2d746e90b5a5
Takenobu Tani <takenobu.hs@gmail.com>
2016-12-25 12:02:22 +0900

    add comment.

--- stgpm-mk1/rtl/lib/ff.v
+++ stgpm-mk1/rtl/lib/ff.v
@@ -1,6 +1,12 @@
+/* -----------------------------------------------------------------------------
+ *
+ * FF
+ *
+ * -------------------------------------------------------------------------- */
 
 
 `include "sp1_common.h"
+
 
 module sp1_ff(
   rst,
@@ -10,13 +16,13 @@ module sp1_ff(
   q
 );
 
-  parameter DW = 32;
+  parameter DW = 32;            // data bit width
 
-  input             rst;
-  input             clk;
-  input             en;
-  input   [DW-1:0]  d;
-  output  [DW-1:0]  q;
+  input             rst;        // reset (1:reset / 0:normal)
+  input             clk;        // clock
+  input             en;         // write enable
+  input   [DW-1:0]  d;          // input data
+  output  [DW-1:0]  q;          // output data
 
   reg     [DW-1:0]  q;
 
