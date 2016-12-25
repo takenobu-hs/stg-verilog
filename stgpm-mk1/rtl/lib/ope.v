b2635c51646978d9ac52206618b680a7afcecd03
Takenobu Tani <takenobu.hs@gmail.com>
2016-12-25 12:19:19 +0900

    add comment.

--- stgpm-mk1/rtl/lib/ope.v
+++ stgpm-mk1/rtl/lib/ope.v
@@ -1,8 +1,16 @@
+/* -----------------------------------------------------------------------------
+ *
+ * Operational logics
+ *  - adder
+ *  - incrementer/decrementer
+ *
+ * -------------------------------------------------------------------------- */
 
 
 `include "sp1_common.h"
 
 
+/* adder */
 module sp1_adder(
   a,
   b,
@@ -10,12 +18,12 @@ module sp1_adder(
   c
 );
 
-  parameter DW = 32;
+  parameter DW = 32;            // data bit width
 
-  input   [DW-1:0]  a;
-  input   [DW-1:0]  b;
-  output  [DW-1:0]  y;
-  output            c;    // carry
+  input   [DW-1:0]  a;          // input operand a
+  input   [DW-1:0]  b;          // input operand b
+  output  [DW-1:0]  y;          // added data
+  output            c;          // carry bit
 
   assign {c, y} = a + b;
 
@@ -23,17 +31,18 @@ endmodule
 
 
 
+/* incrementer */
 module sp1_incr(
   a,
   y,
   c
 );
 
-  parameter DW = 32;
+  parameter DW = 32;            // data bit width
 
-  input   [DW-1:0]  a;
-  output  [DW-1:0]  y;
-  output            c;    // carry
+  input   [DW-1:0]  a;          // input operand
+  output  [DW-1:0]  y;          // incremented data
+  output            c;          // carry bit
 
   assign {c, y} = a + 1;
 
@@ -41,17 +50,18 @@ endmodule
 
 
 
+/* decrementer */
 module sp1_decr(
   a,
   y,
   b
 );
 
-  parameter DW = 32;
+  parameter DW = 32;            // data bit width
 
-  input   [DW-1:0]  a;
-  output  [DW-1:0]  y;
-  output            b;    // carry
+  input   [DW-1:0]  a;          // input operand
+  output  [DW-1:0]  y;          // decremented data
+  output            b;          // borrow bit
 
   assign {b, y} = a - 1;
 
