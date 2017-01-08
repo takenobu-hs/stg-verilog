5b590d5d672c9a2c84eb9bbbf95b58f178d946d5
Takenobu Tani <takenobu.hs@gmail.com>
2017-01-08 21:36:55 +0900

    add parameters for heap modules.

--- stgpm-mk1/rtl/include/sp1_common.h
+++ stgpm-mk1/rtl/include/sp1_common.h
@@ -2,3 +2,15 @@
 
 // `timescale 1ns / 100ps
 
+
+/* basic configuration */
+`define SP1_BYTE_BITS            8
+`define SP1_WORD_ABITS           2
+`define SP1_WORD_WIDTH          (`SP1_BYTE_BITS << `SP1_WORD_ABITS)
+`define SP1_WORD_BYTES          (`SP1_WORD_WIDTH / `SP1_BYTE_BITS)
+
+/* heap configuration */
+`define SP1_HEAP_ABITS          12
+`define SP1_HEAP_BYTES          (1<<`SP1_HEAP_ABITS)
+`define SP1_HEAP_WORDS          (`SP1_HEAP_BYTES / `SP1_WORD_BYTES)
+
